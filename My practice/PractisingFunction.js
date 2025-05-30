b6a42496da608668cef9a319b7ab6db7453c0444
GiftedGift <askhamegift@gmail.com>
2025-05-30 02:36:54 +0100

    added folder for personal practice

---
+++ My practice/PractisingFunction.js
@@ -0,0 +1,16 @@
+function sayHello(){
+	console.log("Hello,JavaScript!");
+}
+sayHello()
+
+const multiply = function(a, b){
+	console.log(a * b);
+
+}
+multiply(2, 3);
+
+
+const makeNoise = function(){
+	console.log("pling!");
+};
+makeNoise();
