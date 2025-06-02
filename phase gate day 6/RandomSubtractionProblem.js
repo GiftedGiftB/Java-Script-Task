f7645157e086e7b9095142791bda13b3b31e39b6
GiftedGift <askhamegift@gmail.com>
2025-06-02 17:01:24 +0100

    random subtraction problem

---
+++ phase gate day 6/RandomSubtractionProblem.js
@@ -0,0 +1,26 @@
+console.log("Lets play a random subtraction problem")
+
+function subtrsction(firstNumber,secondNumber){
+
+	let firstNumber = Math.floor(Math.random() * 20);
+	let secondNumber = Math.floor(Math.random() * firstNumber);
+
+	let result = firstNumber - secondNumber;
+
+	#let result = console.log (firstNumber - secondNumber);
+
+	return result;
+};
+#console.log (subtrsction());
+
+let counter = 0;
+
+let userResult = prompt("whats the answer: ")
+
+
+while(counter <= userResult)
+	
+
+	}
+	
+
