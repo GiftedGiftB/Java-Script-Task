32cd2b804f36e0fa83e9c07f5004e2a02ec59b72
GiftedGiftB <asekhamegift@gmail.com>
2025-06-19 01:13:54 +0100

    learnt how to print 1 - 10 using loop alone

---
+++ My practice/ForLoopPractice.js
@@ -0,0 +1,21 @@
+ 	for(let count = 1; count <= 10; count++)
+		console.log(count + " ");
+
+	
+		
+	/* for(int count = 2; count <= 10; count += 2)
+		System.out.print(count + " "); */
+
+
+	/* for(int count = 1; count <= 10; count += 2)
+		System.out.print(count + " "); */
+
+	
+	 /* for(int count = 4; count <= 10; count += 4)
+		System.out.print(count + " "); */
+
+
+	 /* for(int count = 4; count <= 10; count += 4)
+		 for(int counter = 1; counter <= 5; counter++)
+		
+		System.out.print(count + " "); */
